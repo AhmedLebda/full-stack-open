423f4679c944ff7d0849012bd2fc79b916b74d58
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-10-03 18:27:02 +0300

    part-13: Exercise 13.4

--- part-13/blog-postgres/index.js
+++ part-13/blog-postgres/index.js
@@ -30,3 +30,32 @@ app.post("/api/blogs", async (req, res) => {
 		res.status(500).json({ error: "Server error" });
 	}
 });
+
+app.delete("/api/blogs/:id", async (req, res) => {
+	try {
+		const blog = await Blog.findByPk(req.params.id);
+		if (!blog) {
+			return res.status(404).json({ error: "Blog not found" });
+		}
+		await blog.destroy();
+		res.status(204).end();
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Server error" });
+	}
+});
+
+app.patch("/api/blogs/:id", async (req, res) => {
+	try {
+		const blog = await Blog.findByPk(req.params.id);
+		if (!blog) {
+			return res.status(404).json({ error: "Blog not found" });
+		}
+		blog.likes += 1;
+		await blog.save();
+		res.json(blog);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Server error" });
+	}
+});
